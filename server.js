ffa85fad18c95a5be30a7000a3781de7473e4f56
Erika <erikamiwa612@gmail.com>
2020-04-01 19:28:24 -0700

    test

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
